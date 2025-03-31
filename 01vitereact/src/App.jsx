2e7c09fa79e59d6532b7b7e836e582d0d714b31d
Shubhankar1211 <155457074+Shubhankar1211@users.noreply.github.com>
2025-03-31 17:45:22 +0000

    learne dabout how to html elment can be interacted with vite

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,8 +1,16 @@
+import First from "./first" 
+
 function App() {
   
 
   return (
-    <h1>hellow world1</h1>
+   // <h1>hellow world1</h1> // ye hamen delete or comment kyo kiya h iska conscept h jsx ka rule ye jsx ka ki app ek hi elment return kar skate ho ise overcome karne ke loiye use div bna do kyo ki h to vo single lement hi
+   // <> isko fragment bolte h 
+   <> 
+    <First/>
+    <h1>hello</h1>
+   </>
+    
   )
 }
 
---
+++ 01vitereact/src/first.jsx
@@ -0,0 +1,8 @@
+function First(){
+    return(
+        <h3>fucntion which directly combined</h3>
+    )
+}
+
+export default First // agr ham small case use karnege to error ayegi or kuch nahi dekha isliye hame uppercase me likhna padta h 
+// ye jo hamne fucntion bnya h use ham app.js me bhej rahe h
