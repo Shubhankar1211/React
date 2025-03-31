b93e66b7ec0c3ae593979762c085451159923382
Shubhankar1211 <155457074+Shubhankar1211@users.noreply.github.com>
2025-03-31 18:04:19 +0000

    learned about how to maek components in react app and vite

--- 01bsaicreact/src/App.js
+++ 01bsaicreact/src/App.js
@@ -1,8 +1,12 @@
-
+import Frist from "./First"; // yha  us new fucntion ko import kiya
 
 function App() {
   return (
-    <h1>hello world</h1> // app yha par ek fucntion h jo html ko return kar raha h or isko ham render  kara rahea h index.js me
+    <>
+       <h1>hello world</h1> // app yha par ek fucntion h jo html ko return kar raha h or isko ham render  kara rahea h index.js me
+       <Frist/>
+    </>
+    
   );
 }
 
---
+++ 01bsaicreact/src/First.js
@@ -0,0 +1,14 @@
+// yha ek function bya use export kiya 
+function First(){
+    return(
+        <h1>utkarsh don</h1>
+    )
+}
+
+export default First
+
+
+/// basic learning ye jo ham fucntion export kar rhae h ye hi component h
+//  chae ham vite se kaam kar eya react se kaam kare jo bhi fucntion baoo  use hamesh capital se start karoo 
+// or jo bhi file bna rahe ho use bhi captial se hi surrat karo
+//or kuch library forse karti h ki unka file ka nme jsx hi rkahe
